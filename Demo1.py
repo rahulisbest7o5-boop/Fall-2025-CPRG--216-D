671c515a70e7d809737bb7158934588dc00ca4d1
rahulisbest7o5-boop <rahulisbest7o5@gmail.com>
2025-09-16 10:13:18 -0600

    new

---
+++ Demo1.py
@@ -0,0 +1,16 @@
+x=3
+y=4.5
+message = "hello"
+msg = 'hola'
+
+print(x+1)
+print(y)
+print(message)
+print(msg)
+
+old = True
+young=False
+yx=old and young
+print(yx)
+yx=old or young
+print(yx)
---
+++ demo2.py
@@ -0,0 +1,18 @@
+jam = 65
+well=9.6
+y=2/5
+chart="welcome"
+z= jam + well
+print_65 = False
+
+
+print(z)
+print(jam)
+print(print_65)
+print(chart)
+print(y)
+
+
+
+
+9
